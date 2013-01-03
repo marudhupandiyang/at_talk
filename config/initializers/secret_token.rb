916fbded88ef64e9fc1e376986a10c1d2c3de82a
Marudhu Pandiyan G <marudhupandiyang@gmail.com>
2013-01-03 20:11:39 +0530

    Created App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtTalk::Application.config.secret_token = '869e90bef67ad6e0333161d8252ee79cc6cc2feab1809cc1f2a07026c0048e1b743b841a3263cb31e1dd5b7585a424a3bca4b84b16233dc6863f9731d61f61cb'
